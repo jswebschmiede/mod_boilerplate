bdaf37e90c1ff919cb9305993d2b8f3fd5d99756
jswebschiede <schoeneburg.j@googlemail.com>
2025-02-01 16:51:44 +0100

    Add web asset management for module scripts and styles

--- src/tmpl/default.php
+++ src/tmpl/default.php
@@ -2,34 +2,36 @@
 
 /**
  * @package     Joomla.Site
- * @subpackage  Module.Example
+ * @subpackage  Module.Boilerplate
  *
  * @link        https://www.joomla.org
  * @license     GNU General Public License version 2 or later; see LICENSE
  */
 
+use Joomla\CMS\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
 
 defined('_JEXEC') or die;
 
-$modId = 'mod-example' . $module->id;
+$modId = 'mod-boilerplate' . $module->id;
+
+/** @var Joomla\CMS\WebAsset\WebAssetManager $wa */
+$wa = Factory::getApplication()->getDocument()->getWebAssetManager();
+$wa->getRegistry()->addExtensionRegistryFile('mod_boilerplate');
+$wa->useScript('module.boilerplate.site');
+$wa->useStyle('module.boilerplate.site');
 ?>
 
-
-<div id="<?php echo $modId; ?>" class="mod-example example">
+<div id="<?php echo $modId; ?>" class="mod-boilerplate boilerplate">
 	<?php echo $msg; ?>
 
-	<?php if (!empty($items)) : ?>
+	<?php if (!empty($items)): ?>
 		<div>
-			<?php foreach ($items as $item) : ?>
+			<?php foreach ($items as $item): ?>
 				<figure>
 					<?php echo HTMLHelper::image($item->itemboxImage, $item->itemboxTitle, ['class' => 'image']) ?>
 				</figure>
 			<?php endforeach; ?>
 		</div>
 	<?php endif; ?>
-
-	<div>
-		<?php echo HTMLHelper::link('/', 'title'); ?>
-	</div>
 </div>
